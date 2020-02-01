4e3b0c7a0590b25cf58cf70077b0c3ef923651c9
mbj-junior <mbj_junior@hotmail.com>
2020-01-31 23:01:42 -0300

    resolução do desafio

--- desafioSemanal/nodejs-1/src/index.js
+++ desafioSemanal/nodejs-1/src/index.js
@@ -12,14 +12,18 @@ const parsedFile = file
 //Quantas nacionalidades (coluna `nationality`) diferentes existem no arquivo? 
 const q1 = () => {
     const result = new Set();
+    
     parsedFile.forEach(line => line[14] && result.add(line[14]))
+
     return result.size
 }
     
 //Quantos clubes (coluna `club`) diferentes existem no arquivo?
 const q2 = () => {
     const result = new Set();
+
     parsedFile.forEach(line => line[3] && result.add(line[3]))
+
     return result.size
 }
 
@@ -33,27 +37,110 @@ const q3 = () => {
         result.push(a[0])
     }
 
-    console.log(result)
-
     return result
 }
 
-//Quem são os top 10 jogadores que ganham mais dinheiro (utilize as colunas `full_name` e `eur_wage`)?
+//Quem são os top 10 jogadores que ganham mais dinheiro (utilize as colunas `full_name [2]` e `eur_wage [17]`)?
 const q4 = () => {
      const result = []
+     
+     parsedFile.forEach(line => {
+        if(line[17]){
+            result.push({fname:line[2], eurWage:line[17]})
+         }
+     })
 
-     for
+     result.sort(compareEurWage)
 
 
-     return result
-    
+     let nomeDezPrimeirosSalario = [10]
+     
+     for(let i = 0; i<10; i++){
+        nomeDezPrimeirosSalario[i] = result[i].fname
+     }
 
+     return nomeDezPrimeirosSalario
+}
+
+
+function compareEurWage (a,b){
+
+    if(parseInt(a.eurWage)>parseInt(b.eurWage)){
+        return -1
+    } 
+    if(parseInt(a.eurWage)<parseInt(b.eurWage)){
+        return 1
+    }
+
+    return 0
 }
 
 //Quem são os 10 jogadores mais velhos (use como critério de desempate o campo `eur_wage`)?
-const q5 = () => null
+const q5 = () => {
+
+    const result = []
+     
+     parsedFile.forEach(line => {
+        if(line[6]){
+            result.push({fname:line[2], eurAge:line[6], eurWage:line[17]})
+         }
+     })
+
+     result.sort(compareAge)
+
+     let nomeDezPrimeirosIdade = [10]
+     
+     for(let i = 0; i<10; i++){
+        nomeDezPrimeirosIdade[i] = result[i].fname
+     }
+
+     return nomeDezPrimeirosIdade
+}
+
+
+
+function compareAge (a,b){
+
+    if(parseInt(a.eurAge)>parseInt(b.eurAge)){
+        return -1
+    } 
+    if(parseInt(a.eurAge)<parseInt(b.eurAge)){
+        return 1
+    }
+    if(parseInt(a.eurAge)===parseInt(b.eurAge)){
+        return compareEurWage(a,b)
+    }
+
+    return 0
+}
+
 
 //Conte quantos jogadores existem por idade. Para isso, construa um mapa onde as chaves são as idades e os valores a contagem.
-const q6 = () => null
+const q6 = () => {
+    //Atribuir um mapa
+    let meuMapa = new Map()
+
+    //percorer o arquivo 
+    parsedFile.forEach(line => {
+        //pegar a idade do jogador da linha em questão
+        let idade = line[6]
+
+        if (idade){
+            //verificar se existe a idade como uma chave
+            if(meuMapa.has(idade)){
+                // se existir, somar +1 ao valor 
+                meuMapa.set(idade, meuMapa.get(idade)+1)
+            }else{
+                //se não existir, addicionar a chave com o valor 1
+                meuMapa.set(idade,1)
+            }
+        }
+
+                        
+    })
+    
+    return{meuMapa}
+}
+
 
 module.exports = { q1, q2, q3, q4, q5, q6 }
